ab61222ea4f35b1b6fe4f0ef976062e2d61ccea9
Tomasz Jarych <tjarych@gmail.com>
2018-09-05 16:19:38 +0200

    Spring01Hibernate - Dzień 4 Wszystko

--- src/main/java/pl/coderslab/Repository/CustomRepository/BookRepositoryImpl.java
+++ src/main/java/pl/coderslab/Repository/CustomRepository/BookRepositoryImpl.java
@@ -2,27 +2,42 @@ package pl.coderslab.Repository.CustomRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceUnit;
 import javax.persistence.Query;
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class BookRepositoryImpl implements BookRepositoryCustom {
 
-    @PersistenceUnit
-    private EntityManagerFactory emFactory;
+//    @PersistenceUnit
+//    private EntityManagerFactory emFactory;
+//    
+//    
+//
+//    // problem z błędem TransactionRequiredException:
+//    @Override
+//    public void resetRating(int rating) {
+//
+//	EntityManager em = emFactory.createEntityManager();
+//	em.getTransaction().begin();
+//	Query queryp = em.createQuery("UPDATE Book b SET b.rating =:rating");
+//	queryp.setParameter("rating", rating).executeUpdate();
+//	em.getTransaction().commit();
+//	em.close();
+//    }
+    
+    @PersistenceContext
+    private EntityManager em;
 
-    // problem z błędem TransactionRequiredException:
     @Override
+    @Transactional
     public void resetRating(int rating) {
-
-	EntityManager em = emFactory.createEntityManager();
-	em.getTransaction().begin();
 	Query queryp = em.createQuery("UPDATE Book b SET b.rating =:rating");
 	queryp.setParameter("rating", rating).executeUpdate();
-	em.getTransaction().commit();
-	em.close();
+	
     }
 
 }
